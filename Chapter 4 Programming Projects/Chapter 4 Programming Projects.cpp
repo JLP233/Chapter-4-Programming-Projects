348630ee8d0becf7d4dbed8f24c398586e2f5688
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-03-26 18:07:44 -0400

    Input intial if else if logic for determining number of days based on given month

--- Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
+++ Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
@@ -5,18 +5,35 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	float Month;
+	float Year;
+	float DaysInMonth;
+	
+   cout << "Enter a month (1-12):\n";
+   cin >> Month;
+   cout << "Enter a year:\n";
+   cin >> Year;
+
+   if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
+   {
+	   DaysInMonth == 31;
+   }
+   else if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
+   {
+	   DaysInMonth == 30;
+   }
+   else if (Month == 2)
+   {
+	   DaysInMonth == 28;
+   }
+   else cout << "Invalid Entry. Please enter a month (1-12) and year as integer values." << endl;
+	   return 1;
+   
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
