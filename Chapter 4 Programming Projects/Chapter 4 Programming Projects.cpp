5e2b045cca0b58299cdd005014b89292edbd3f2b
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-03-26 18:57:35 -0400

    added T/F LeapYear function to properly determine the number of days in month 2

--- Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
+++ Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
@@ -7,12 +7,16 @@
 
 using namespace std;
 
+bool LeapYear(int Year) {
+	return (Year % 4 == 0 && (Year % 100 != 0 || Year % 400 == 0));
+}
+
 int main()
 {
 	float Month;
 	float Year;
 	float DaysInMonth;
-	
+
    cout << "Enter a month (1-12):\n";
    cin >> Month;
    cout << "Enter a year:\n";
@@ -20,20 +24,30 @@ int main()
 
    if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
    {
-	   DaysInMonth == 31;
+	   DaysInMonth = 31;
    }
    else if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
    {
-	   DaysInMonth == 30;
+	   DaysInMonth = 30;
    }
    else if (Month == 2)
    {
-	   DaysInMonth == 28;
+	   if (LeapYear (Year)) {
+		   DaysInMonth = 29; 
+
+	   }
+	   else {
+		   DaysInMonth = 28;
+	   }
    }
-   else cout << "Invalid Entry. Please enter a month (1-12) and year as integer values." << endl;
+   else {
+	   cout << "Invalid Entry. Please enter a month (1-12) and year as integer values." << endl;
 	   return 1;
-   
+   }
+   cout << "Number of Days in Selected Month:";
+   cout << DaysInMonth;
 
 
+   return 0;
 }
 
