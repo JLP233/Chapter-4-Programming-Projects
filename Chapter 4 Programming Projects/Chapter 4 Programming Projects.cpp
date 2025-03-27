ab4ad95395597a840d0b748a829774cc60165599
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-03-26 23:16:50 -0400

    fixed if/or statement on rectangle calculation to if/and

--- Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
+++ Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
@@ -20,84 +20,92 @@ int main()
 	float TriangleBase;
 	float TriangleHeight;
 
-	
-   cout << "Geometry Calculator:\n";
-   cout << "1. Calculate the Area of a Circle\n";
-   cout << "2. Calculate the Area of a Rectangle\n";
-   cout << "3. Calculate the Area of a Triangle\n";
-   cout << "4. Quit\n";
-   cout << "Enter your choice(1-4):\n";
-   cin >> FunctionChoice;
 
-  
-   if (FunctionChoice == 1)
-   {
-	   cout << "Calculate the Area of a Circle\n";
-	   cout << "Enter circle radius:\n";
-	   cin >> CircleRadius;
-	   if (CircleRadius >= 0)
-	   {
-		   AreaOfShape = CircleRadius * CircleRadius * 3.14159;
-	   }
+	cout << "Geometry Calculator:\n";
+	cout << "1. Calculate the Area of a Circle\n";
+	cout << "2. Calculate the Area of a Rectangle\n";
+	cout << "3. Calculate the Area of a Triangle\n";
+	cout << "4. Quit\n";
+	cout << "Enter your choice(1-4):\n";
+	cin >> FunctionChoice;
 
-	   else
-	   {
-		   cout << "Input Error.You must enter a positive value.\n";
-		   return 1;
-	   }
-   }
 
-   else if (FunctionChoice == 2)
-   {
-	   cout << "Calculate the Area of a Rectangle\n";
-	   cout << "Enter rectangle length:\n";
-	   cin >> RectangleLength;
-	   cout << "Enter rectangle width:\n";
-	   cin >> RectangleWidth; 
-	   if (RectangleLength <= 0 || RectangleWidth <= 0)
-	   {
-		   cout << "Input Error.You must enter a positive value.\n";
-		   return 1;
-	   }
-		   
-	   else
-	   {
-		   AreaOfShape = RectangleLength * RectangleWidth;
-	   }
-   }
+	if (FunctionChoice == 1)
+	{
+		cout << "Calculate the Area of a Circle\n";
+		cout << "Enter circle radius:\n";
+		cin >> CircleRadius;
+		if (CircleRadius >= 0)
+		{
+			AreaOfShape = CircleRadius * CircleRadius * 3.14159;
+		}
 
-   else if (FunctionChoice == 3)
-   {
-	   cout << "Calculate the Area of a Triangle\n";
-	   cout << "Enter triangle base length:\n";
-	   cin >> TriangleBase;
-	   cout << "Enter triangle height:\n";
-	   cin >> TriangleHeight;
-	   if (TriangleBase <= 0 || TriangleHeight <= 0)
-	   {
-		   cout << "Input Error.You must enter a positive value.\n";
-	   }
+		else
+		{
+			cout << "Input Error.You must enter a positive value.\n";
+			return 1;
+		}
+	}
 
-	   else
-	   {
-		   AreaOfShape = TriangleBase * TriangleHeight * .5;
-	   }
-   }
+	else if (FunctionChoice == 2)
+	{
+		cout << "Calculate the Area of a Rectangle\n";
+		cout << "Enter rectangle length:\n";
+		cin >> RectangleLength;
+		cout << "Enter rectangle width:\n";
+		cin >> RectangleWidth; 
+		if (RectangleLength >= 0 && RectangleWidth >= 0)
+		{
+			AreaOfShape = RectangleLength * RectangleWidth; 
+		}
 
-   else if (FunctionChoice == 4)
-   {
-	   cout << "Goodbye";
-	   return 0;
-   }
+		else
+		{
+			cout << "Input Error.You must enter a positive value.\n";
+			return 1;
+		}
+	}
 
-   else 
-   {
-	   cout << "Invalid selection. Please pick an option 1-4.\n";
-	   
-   }
+	else if (FunctionChoice == 3)
+	{
+		cout << "Calculate the Area of a Triangle\n";
+		cout << "Enter triangle base length:\n";
+		cin >> TriangleBase;
+		cout << "Enter triangle height:\n";
+		cin >> TriangleHeight;
+		if (TriangleBase <= 0 || TriangleHeight <= 0)
+		{
+			cout << "Input Error.You must enter a positive value.\n";
+			return 1;
+		}
 
-   cout << "Area of shape:\n";
-   cout << AreaOfShape;
+		else
+		{
+			AreaOfShape = TriangleBase * TriangleHeight * .5;
+		}
+	}
+
+	else if (FunctionChoice == 4)
+	{
+		cout << "Goodbye";
+		return 0;
+	}
+
+	else
+	{
+		cout << "Invalid selection. Please pick an option 1-4.\n";
+
+	}
+	if (AreaOfShape > 0)
+	{
+	cout << "Area of shape:\n";
+	cout << AreaOfShape;
+}
+	else
+	{
+		cout << "Error.Please Try Agian.";
+		return 1;
+	}
 
    return 0;
 }
