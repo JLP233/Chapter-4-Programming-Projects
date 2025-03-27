5133d7ee5b94c88fbdcd321908fa0af9b77950c1
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-03-26 21:20:21 -0400

    Finished adding if else if statements for different weight ranges as well as final output display

--- Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
+++ Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
@@ -7,10 +7,12 @@
 
 using namespace std;
 
+
 int main()
 {
 	float PackageWeight;
 	float MilesToBeShipped;
+	float TotalCostToShip;
 
 	
    cout << "Enter package weight (kg):\n";
@@ -18,28 +20,30 @@ int main()
    cout << "Enter number of miles package needs to be shipped (mi):\n";
    cin >> MilesToBeShipped;
 
-   if (PackageWeight <= 2)
+   if (PackageWeight <= 2 && PackageWeight >= 0)
    {
-	   MilesToBeShipped * 1.1;
+	   MilesToBeShipped * 1.1 = TotalCostToShip;
    }
    else if (PackageWeight >= 2 && PackageWeight <= 6)
    {
-	   MilesToBeShipped * 2.2;
+	   MilesToBeShipped * 2.2 = TotalCostToShip;
    }
    else if (PackageWeight >= 6 && PackageWeight <= 10)
    {
-	   MilesToBeShipped * 3.7;
+	   MilesToBeShipped * 3.7 = TotalCostToShip;
    }
    else if (PackageWeight >= 10 && PackageWeight <= 20)
    {
-	   MilesToBeShipped * 4.8
+	   MilesToBeShipped * 4.8 = TotalCostToShip;
    }
    else {
-	   cout << "Invalid Entry. Please enter a month (1-12) and year as integer values." << endl;
+	   cout << "Package exceeds 20kg weight limit." << endl;
 	   return 1;
    }
-   cout << "Number of Days in Selected Month:";
-   cout << DaysInMonth;
+
+
+   cout << "Total Cost to Ship";
+   cout << "$" << TotalCostToShip;
 
 
    return 0;
