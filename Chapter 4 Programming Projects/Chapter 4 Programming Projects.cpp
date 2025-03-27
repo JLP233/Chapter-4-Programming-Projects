51ecadb746b37bbdf665026cd010b2a9ba524534
Jeremiah Penney <Jeremiahpenney9@gmail.com>
2025-03-26 21:01:13 -0400

    Started adding basic if else if statements for various weight ranges and associated prices per 500 miles

--- Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
+++ Chapter 4 Programming Projects/Chapter 4 Programming Projects.cpp
@@ -1,44 +1,38 @@
 // Chapter 4 Programming Projects.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-// This code calculates the number of days in a specified month when provided a month and year as integer values.
+// This code calculates the shipping charges of packages based on parcel weight (kg) and shipping distance in miles.
 
 
 #include <iostream>
 
 using namespace std;
 
-bool LeapYear(int Year) {
-	return (Year % 4 == 0 && (Year % 100 != 0 || Year % 400 == 0));
-}
-
 int main()
 {
-	int Month;
-	int Year;
-	int DaysInMonth;
+	float PackageWeight;
+	float MilesToBeShipped;
 
-   cout << "Enter a month (1-12):\n";
-   cin >> Month;
-   cout << "Enter a year:\n";
-   cin >> Year;
+	
+   cout << "Enter package weight (kg):\n";
+   cin >> PackageWeight;
+   cout << "Enter number of miles package needs to be shipped (mi):\n";
+   cin >> MilesToBeShipped;
 
-   if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
+   if (PackageWeight <= 2)
    {
-	   DaysInMonth = 31;
+	   MilesToBeShipped * 1.1;
    }
-   else if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
+   else if (PackageWeight >= 2 && PackageWeight <= 6)
    {
-	   DaysInMonth = 30;
+	   MilesToBeShipped * 2.2;
    }
-   else if (Month == 2)
+   else if (PackageWeight >= 6 && PackageWeight <= 10)
    {
-	   if (LeapYear (Year)) {
-		   DaysInMonth = 29; 
-
-	   }
-	   else {
-		   DaysInMonth = 28;
-	   }
+	   MilesToBeShipped * 3.7;
+   }
+   else if (PackageWeight >= 10 && PackageWeight <= 20)
+   {
+	   MilesToBeShipped * 4.8
    }
    else {
 	   cout << "Invalid Entry. Please enter a month (1-12) and year as integer values." << endl;
